f3a12cb49eca633dbeac694ca809f58b85fbd55d
DESKTOP-2056LD4\snigg <navarone1970@gmail.com>
2021-04-13 10:01:26 +0100

    Module 3 - Essential patterns for sustainable test automation - tutorial 1 - immutable constructor dog

--- src/main/java/serenitylabs/tutorials/vetclinic/domain/Dog.java
+++ src/main/java/serenitylabs/tutorials/vetclinic/domain/Dog.java
@@ -3,32 +3,40 @@ package serenitylabs.tutorials.vetclinic.domain;
 import java.time.LocalDateTime;
 
 public class Dog {
-    private String name;
+    private final String name;
+    private final String breed;
+    private final LocalDateTime dateOfBirth;
+    private String favouriteFood;
 
-    private String breed;
-    private LocalDateTime dateOfBirth;
+    public Dog(String name, String breed, LocalDateTime dateOfBirth) {
+        this.name = name;
+        this.breed = breed;
+        this.dateOfBirth = dateOfBirth;
+        this.favouriteFood = null;
+    }
 
-    public void setName(String fido) {
-        this.name = fido;
+    public Dog(String name, String breed, LocalDateTime dateOfBirth, String favouriteFood) {
+        this.name = name;
+        this.breed = breed;
+        this.dateOfBirth = dateOfBirth;
+        this.favouriteFood = favouriteFood;
     }
 
     public String getName() {
         return name;
     }
 
-    public void setBreed(String breed) {
-        this.breed = breed;
-    }
-
     public String getBreed() {
         return breed;
-    }
-
-    public void setDateOfBirth(LocalDateTime dateOfBirth) {
-        this.dateOfBirth = dateOfBirth;
     }
 
     public LocalDateTime getDateOfBirth() {
         return dateOfBirth;
     }
+
+
+    public String getFavouriteFood() {
+        return favouriteFood;
+    }
+
 }
--- src/test/java/serenitylabs/tutorials/vetclinic/domain/WhenCreatingANewDog.java
+++ src/test/java/serenitylabs/tutorials/vetclinic/domain/WhenCreatingANewDog.java
@@ -10,15 +10,21 @@ public class WhenCreatingANewDog {
 
     @Test
     public void it_should_have_a_name() throws Exception {
-        Dog fido = new Dog();
         LocalDateTime birthday = LocalDateTime.now();
 
-        fido.setName("Fido");
-        fido.setBreed("Poodle");
-        fido.setDateOfBirth(birthday);
+        Dog fido = new Dog("Fido","Poodle",birthday);
 
         Assert.assertEquals("Fido",fido.getName());
         Assert.assertEquals("Poodle",fido.getBreed());
         Assert.assertEquals(birthday,fido.getDateOfBirth());
+    }
+
+    @Test
+    public void it_should_have_an_optional_favourite_food() throws Exception {
+        LocalDateTime birthday = LocalDateTime.now();
+
+        Dog fido = new Dog("Fido","Poodle",birthday,"pizza");
+
+        Assert.assertEquals("pizza",fido.getFavouriteFood());
     }
 }
