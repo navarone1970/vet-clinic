692fef3f58ce21962055c632d4692bafb3abea13
DESKTOP-2056LD4\snigg <navarone1970@gmail.com>
2021-04-13 12:29:59 +0100

    Module 3 - kata/builder/start - Dog Breeder - attempt 2

--- src/test/java/serenitylabs/tutorials/vetclinic/domain/WhenWeCreateANewDog.java
+++ src/test/java/serenitylabs/tutorials/vetclinic/domain/WhenWeCreateANewDog.java
@@ -2,21 +2,20 @@ package serenitylabs.tutorials.vetclinic.domain;
 
 import org.junit.Assert;
 import org.junit.Test;
+import serenitylabs.tutorials.vetclinic.domain.dog.Dog;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 
 public class WhenWeCreateANewDog {
+
     @Test
     public void a_new_dog_should_have_a_name(){
-        LocalDate birthday = LocalDate.of(2015,10,13);
 
-        Dog fido = Dog.called("Fido")
-                .ofBreed("Labrador")
-                .bornOn(birthday);
+        LocalDate birthday = LocalDate.now();
+        Dog fido = new Dog("Fido","Labrador",birthday);
 
-        Assert.assertEquals("Fido",fido.getName());
-        Assert.assertEquals(LocalDate.of(2015,10,13),fido.getDateOfBirth());
+        Assert.assertEquals("Fido", fido.getName());
+        Assert.assertEquals(LocalDate.now(),fido.getDateOfBirth());
         Assert.assertEquals("Labrador",fido.getBreed());
     }
 }
