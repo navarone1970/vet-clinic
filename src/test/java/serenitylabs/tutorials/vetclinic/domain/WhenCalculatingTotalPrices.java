2f62586b9e29d9fda1c82b92e0e2e8ff1c45f7e6
DESKTOP-2056LD4\snigg <navarone1970@gmail.com>
2021-04-14 11:22:23 +0100

    Module 3 - kata/builder/start - Hamcrest matchers - tutorial

--- src/test/java/serenitylabs/tutorials/vetclinic/domain/WhenCalculatingTotalPrices.java
+++ src/test/java/serenitylabs/tutorials/vetclinic/domain/WhenCalculatingTotalPrices.java
@@ -4,6 +4,10 @@ import org.junit.Test;
 
 import org.junit.Assert;
 
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.greaterThan;
+
 public class WhenCalculatingTotalPrices {
 
     @Test
@@ -16,5 +20,31 @@ public class WhenCalculatingTotalPrices {
 
         // THEN
         Assert.assertEquals(120, totalPrice);
+
+        assertThat(totalPrice,greaterThan(150));
+    }
+
+    @Test
+    public void total_consultation_price_should_include_20_percent_tax_Hamcrest() throws Exception {
+        // GIVEN
+        int netPrice = 100;
+
+        // WHEN
+        int totalPrice = TotalConsultationPrice.includingTax().forANetPriceOf(netPrice);
+
+        // THEN
+        assertThat(totalPrice,equalTo(120));
+    }
+
+    @Test
+    public void total_consultation_price_should_include_20_percent_tax_greaterThan_Hamcrest() throws Exception {
+        // GIVEN
+        int netPrice = 100;
+
+        // WHEN
+        int totalPrice = TotalConsultationPrice.includingTax().forANetPriceOf(netPrice);
+
+        // THEN
+        assertThat(totalPrice,greaterThan(100));
     }
 }
